d0243bc42ff0315cd10c7f548f32959e71b24386
ross39 <Rheaney26@gmail.com>
2020-02-26 17:42:11 +0000

    More work

--- Assets/Entities/Player/PlayerController.cs
+++ Assets/Entities/Player/PlayerController.cs
@@ -2,13 +2,16 @@
 using System.Collections;
 using Thalmic.Myo;
 using LockingPolicy = Thalmic.Myo.LockingPolicy;
+using UnlockType = Thalmic.Myo.UnlockType;
 using Pose = Thalmic.Myo.Pose;
 using VibrationType = Thalmic.Myo.VibrationType;
 
 
 public class PlayerController : MonoBehaviour {
 
+	public ThalmicMyo thalmicMyo;
 	public GameObject myo = null;
+	private Pose _lastPose = Pose.Unknown;
 	public GameObject laser;
 	public float projectileSpeed = 10;
 	public float projectileRepeatRate = 0.2f;
@@ -42,33 +45,43 @@ public class PlayerController : MonoBehaviour {
 	void Start(){
 		Camera camera = Camera.main;
 		float distance = transform.position.z - camera.transform.position.z;
-		xmin = camera.ViewportToWorldPoint(new Vector3(0,0,distance)).x + padding;
-		xmax = camera.ViewportToWorldPoint(new Vector3(1,1,distance)).x - padding;
+		xmin = camera.ViewportToWorldPoint(new UnityEngine.Vector3(0,0,distance)).x + padding;
+		xmax = camera.ViewportToWorldPoint(new UnityEngine.Vector3(1,1,distance)).x - padding;
 	}
 
 	void Fire(){
-		GameObject beam = Instantiate(laser, transform.position, Quaternion.identity) as GameObject;
-		beam.GetComponent<Rigidbody2D>().velocity = new Vector3(0, projectileSpeed, 0);
+		GameObject beam = Instantiate(laser, transform.position, UnityEngine.Quaternion.identity) as GameObject;
+		beam.GetComponent<Rigidbody2D>().velocity = new UnityEngine.Vector3(0, projectileSpeed, 0);
 		AudioSource.PlayClipAtPoint(fireSound, transform.position);
 	}
 	void Update () {
-		        ThalmicMyo thalmicMyo = myo.GetComponent<ThalmicMyo>();
+		ThalmicMyo thalmicMyo = myo.GetComponent<ThalmicMyo>();
 
+# Need to set up the poses to work with the current keyboard inputs.
+# Have a settings menu where the user can chose between the twO?
 
-		if(Input.GetKeyDown(KeyCode.Space)){
+		if(thalmicMyo.pose == Pose.Fist){
+			thalmicMyo.Vibrate(VibrationType.Short);
 			InvokeRepeating("Fire", 0.0001f, projectileRepeatRate);
 		}
-		if(Input.GetKeyUp(KeyCode.Space)){
-			CancelInvoke("Fire");
-		}
-		if(Input.GetKey(KeyCode.LeftArrow)){
-			transform.position = new Vector3(
+		//if(Input.GetKeyDown(KeyCode.Space)){
+		//InvokeRepeating("Fire", 0.0001f, projectileRepeatRate);
+		//}
+		//if(Input.GetKeyUp(KeyCode.Space)){
+		//CancelInvoke("Fire");
+		//}
+#Waving left
+		if(thalmicMyo.pose == Pose.WaveIn){
+			thalmicMyo.Vibrate(VibrationType.Short);
+			transform.position = new UnityEngine.Vector3(
 				Mathf.Clamp(transform.position.x - speed * Time.deltaTime, xmin, xmax),
 				transform.position.y, 
 				transform.position.z 
 			);
-		}else if (Input.GetKey(KeyCode.RightArrow)){
-			transform.position = new Vector3(
+#Waving right
+		}else if (thalmicMyo.pose == Pose.WaveOut){
+			thalmicMyo.Vibrate(VibrationType.Short);
+			transform.position = new UnityEngine.Vector3(
 				Mathf.Clamp(transform.position.x + speed * Time.deltaTime, xmin, xmax),
 				transform.position.y, 
 				transform.position.z 
